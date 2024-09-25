8284ccc452235d73e945c439cd88ad8225515fa6
kamalahuja21 <kamalahuja21@gmail.com>
2024-09-25 18:02:49 -0400

    Socket programming

--- SockServer.py
+++ SockServer.py
@@ -1,13 +1,18 @@
 import socket
 
-s=socket.socket()
-print('Socket Created')
-s.bind(('localhost',9998))
-s.listen(3)
+try:
+    s=socket.socket()
+    print('Socket Created')
+    s.bind(('localhost',9998))
+    s.listen(3)
 
-while True:
-    c,addr=s.accept()
-    name = c.recv(1024).decode()
-    print("Connected with ",addr,name)
-    c.send(bytes('Welcome to socket programming','utf-8'))
-    c.close()
+    while True:
+        c,addr=s.accept()
+        name = c.recv(1024).decode()
+        print("Connected with ",addr,name)
+        c.send(bytes('Welcome to socket programming','utf-8'))
+        c.close()
+except KeyboardInterrupt:
+    s.close()
+finally:
+    s.close()
